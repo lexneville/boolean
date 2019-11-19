70dee11a364a9c5aee5139c0b9ffc08d967ccb11
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-19 17:54:55 +0000

    reupload

---
+++ boolean.js
@@ -0,0 +1,54 @@
+// let glass = true;
+
+// console.log(glass);
+
+// if(glass) {
+//     console.log("This is true");
+// } else {
+//     console.log("This is false");
+// }
+
+// let glass = "empty";
+
+// if (glass == "full") {
+//     console.log("The glass is full");
+// } else {
+//     console.log("The glass is empty");
+// }
+
+// let number = 2 ;
+
+// if (number === "2") {
+//     console.log("The numbers and data types are the same"); // == just compares the values of variables, === compares the data type as well.
+// } else {
+//     console.log("The numbers or data types are different");
+// }
+
+// let morning = false;
+
+// if (!morning){
+//     console.log("It's late in the day");
+// } else {
+//     console.log("It's morning right now");
+// }
+
+// let number1 = 13;
+// let number2 = 13;
+
+// if (number1 >= number2) {
+//     console.log("This is true");
+// } else {
+//     console.log("This is false");
+// }
+
+// if ( (6 < 9) && !(2 > 5) ){     // The exclamation mark switches a true into a false
+//     console.log("This is true");
+// } else {
+//     console.log("This is false");
+// }
+
+if ( ( 5 < 3) || !(4 > 2)){
+    console.log("One or both of these statements are true");
+} else {
+    console.log("Neither of these statements are true");
+}
